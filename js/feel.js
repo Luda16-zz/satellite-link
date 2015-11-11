add8543218d5f0ea9685603cc3e2907ed85c9e1b
Luda <ldcg1@hotmail.com>
2015-11-11 19:01:30 -0430

    Form fixed at calculos

--- js/feel.js
+++ js/feel.js
@@ -1,1 +1,15 @@
 $(".button-collapse").sideNav();
+
+// "Generic" function
+$.conditionalize = function(sourceSelect, conditionalSelect){
+    var options = conditionalSelect.children(".conditional").clone();
+    
+    sourceSelect.change(function(){
+        var value = $(this).val();                  
+        conditionalSelect.children(".conditional").remove();
+        options.clone().filter("."+value).appendTo(conditionalSelect);
+    }).trigger("change");
+}
+
+// Used like this:
+$.conditionalize($("#firstSelect"), $("#secondSelect"));
